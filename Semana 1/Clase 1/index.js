3838fe175a6e5452f85353e2cde1bcd4550fc9bc
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-04-01 09:58:37 -0600

    Clase 1

---
+++ Semana 1/Clase 1/index.js
@@ -0,0 +1,37 @@
+//Salida -> un comentario de una sola linea
+/*console.log("Hola mundo!");
+
+console.log("Adios");
+
+/*Variables
+JS ES DEBILMENTE TIPADO
+Lower Camel Case
+*/
+
+//declaracion de una variable
+let nombreCompleto;
+nombreCompleto = "Dorian Contreras"; //cadena (string)
+let numero = 10; //numero
+let booleano = true; //boleano (true/false)
+console.log(nombreCompleto);
+console.log(numero);
+console.log(booleano);
+
+//Operadores aritmeticos
+let num = prompt("Ingrese un numero: ");
+console.log(num);
+
+console.log("Suma: " + num+10);
+console.log("Resta: " + num-20);
+console.log("Multiplicacion: " + num*95);
+console.log("Division: " + num/5);
+
+//Comparacion ==, <, >, >=, <=
+console.log(5<20);
+
+//Logicos
+console.log(5<20 && 10==10 && 10<2); //AND y
+console.log(5<20 || 10==10 || 10<2)  //OR o
+
+
+
