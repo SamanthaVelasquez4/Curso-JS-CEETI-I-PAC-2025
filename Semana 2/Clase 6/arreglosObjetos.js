0343cb0eddcca035ed4ac94a180c8c358c32f3d9
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-04-08 10:44:11 -0600

    Clase 6 arreglos y objetos

---
+++ Semana 2/Clase 6/arreglosObjetos.js
@@ -0,0 +1,36 @@
+//crear objeto
+let maria = {
+    "nombre" : "Maria Jose",
+    "apellido": "Flores Gonzales",
+    "edad": 25,
+    "genero": true, //true = mujer y false = hombre
+    "identidad": "0801-2000-32658"
+};
+
+let pedro = {
+    "nombre" : "Pedro",
+    "apellido": "Valladares",
+    "edad": 30,
+    "genero": false, //true = mujer y false = hombre
+    "identidad": "0801-1995-32658"
+};
+
+let personas =[maria,pedro];
+
+//console.log(personas);
+
+personas.push({
+    "nombre" : "Marcos",
+    "apellido": "Villeda",
+    "edad": 30,
+    "genero": false, //true = mujer y false = hombre
+    "identidad": "1701-1995-32658"
+});
+
+//console.log(personas.length);
+
+//console.log(personas[2]);
+
+//buscar la persona con dicha identidad
+let persona = personas.filter(objeto => objeto.genero == false);
+console.log(persona);
