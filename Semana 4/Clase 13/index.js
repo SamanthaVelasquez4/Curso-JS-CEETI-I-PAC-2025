45b9bf4f7a7319012dff7172e73afa248b0eaaa7
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-04-29 08:06:20 -0600

    Finalización de localstorage

--- Semana 4/Clase 13/index.js
+++ Semana 4/Clase 13/index.js
@@ -1,25 +1,45 @@
+
+//verificar el localstorage
+if(localStorage.getItem("usuario")!= null){
+    //ocultar botones
+    document.getElementById("nombre").hidden = true;
+    document.getElementById("edad").hidden = true;
+
+    //obtener usuario
+    let usuarioLocalStorage = JSON.parse(localStorage.getItem("usuario"));
+
+    //actualizaciones
+    document.getElementById("info").innerHTML =`<h2> ${usuarioLocalStorage.nombre}</h2>`;
+    document.body.className = "";
+    document.body.classList.add(usuarioLocalStorage.tema);
+}else{
+    //Mostrar botones
+    document.getElementById("nombre").hidden = false;
+    document.getElementById("edad").hidden = false;
+}
+
 function actualizar(){
     //verificar el localstorage
-    if(localStorage.getItem("usuario")){
+    if(localStorage.getItem("usuario")!= null){
         //ocultar botenes
         document.getElementById("nombre").hidden = true;
         document.getElementById("edad").hidden = true;
 
         //obtener usuario
         let usuarioLocalStorage = JSON.parse(localStorage.getItem("usuario"));
-        if(document.getElementById("tema").value=!""){
+
+        if(document.getElementById("tema").value!=""){
             usuarioLocalStorage.tema = document.getElementById("tema").value;
         }
 
         //actualizaciones
-        document.body.innerHTML += `<h2> ${usuarioLocalStorage.nombre}</h2>`;
+        document.getElementById("info").innerHTML = `<h2> ${usuarioLocalStorage.nombre}</h2>`;
         document.body.className = "";
         document.body.classList.add(usuarioLocalStorage.tema);
+        document.getElementById("tema").value = "";
 
         //guardar usuario
         localStorage.setItem("usuario", JSON.stringify(usuarioLocalStorage));
-
-        
     }else{
         if (document.getElementById("nombre").value!= "" && document.getElementById("edad").value!="" && document.getElementById("tema").value!=""){
             let usuario = {
@@ -27,12 +47,18 @@ function actualizar(){
                 edad : document.getElementById("edad").value,
                 tema : document.getElementById("tema").value
             }
+            //guardar usuario
             localStorage.setItem("usuario", JSON.stringify(usuario));
 
+            //ocultar botones
+            document.getElementById("nombre").hidden = true;
+            document.getElementById("edad").hidden = true;
+
             //actualizaciones
-            document.body.innerHTML += `<h2> ${usuario.nombre}</h2>`;
+            document.getElementById("info").innerHTML = `<h2> ${usuario.nombre}</h2>`;
             document.body.className = "";
             document.body.classList.add(usuario.tema);
+            document.getElementById("tema").value = "";
         }else{
             alert("Llene los campos vacios");
         }
@@ -40,5 +66,3 @@ function actualizar(){
 }
 
 document.getElementById("actualizar").addEventListener("click", actualizar);
-
-    
